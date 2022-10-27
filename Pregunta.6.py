77aaf6e35c410b2d004075a252c7598c16fa7af7
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-10-27 11:49:57 +0200

    third commit

---
+++ Pregunta.6.py
@@ -0,0 +1,6 @@
+from ast import While
+
+
+def modificar():
+    lista = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    While ""
---
+++ listasCEU.py
@@ -0,0 +1,11 @@
+lista = list("abcd")
+def seleccion_de_elementos_de_lista():
+   lista[4]
+   lista[-2]
+def cambio_de_primera_posicion_por_ultima_posicion():
+   lista[1] = "d"
+   lista[4] = "a"
+def eliminacion_del_ultimo_elemento():
+   lista.pop()
+lista2 = list("dbc")
+
